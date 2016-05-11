a70a682c746bb06230819235576b94b29da58d1a
Elsay31 <luc.cazaentre@laposte.net>
2016-05-11 10:28:28 +0200

    Update fonctions.h
    rajout traiter image

--- projet_initial/fonctions.h
+++ projet_initial/fonctions.h
@@ -18,6 +18,7 @@ extern "C" {
         void communiquer(void *arg);
         void deplacer(void *arg);
         void envoyer(void *arg);
+	void traiter_image(void *arg);
 #ifdef	__cplusplus
 }
 #endif
