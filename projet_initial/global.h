6e7b705deabc2b209c331010eb66d5ba6ddd983b
marcyrk <marcy.rakotopare@gmail.com>
2016-05-19 17:21:03 +0200

    Update global.h
    Rajouts suite aux tests

--- projet_initial/global.h
+++ projet_initial/global.h
@@ -16,16 +16,18 @@ extern RT_TASK tconnect;
 extern RT_TASK tmove;
 extern RT_TASK tenvoyer;
 extern RT_TASK ttraiter_image;
-//extern RT_TASK tconnecter_moniteur
-//extern RT_TASK ttraiter_ordre
-//extern RT_TASK tdetecter_arene ;
-//extern RT_TASK tbattery_level;
+extern RT_TASK tdetecter_arene;
+extern RT_TASK tbattery_level;
 //extern RT_TASK tcalcul_pos;
 //extern RT_TASK tfermeture_connexion_robot ;
+extern RT_TASK tconnecter_moniteur;
+//extern RT_TASK ttraiter_ordre;
 
 /* @descripteurs des mutex */
+
 extern RT_MUTEX mutexServeur;
-extern RT_MUTEX mutexEtatMon;
+//extern RT_MUTEX mutexEtatMon;
+extern RT_MUTEX mutexEtat;
 extern RT_MUTEX mutexRobot;
 extern RT_MUTEX mutexMove;
 extern RT_MUTEX mutexArene;
@@ -38,24 +40,21 @@ extern RT_SEM semConnecterRobot;
 extern RT_SEM semConnexionEtablie;
 extern RT_SEM semConnexionEchouee ;
 extern RT_SEM semLancerPosition ;
-extern RT_SEM semTrouverArene ;
-
-
-
-
-
+extern RT_SEM semTrouverArene;
 
 /* @descripteurs des files de messages */
 extern RT_QUEUE queueMsgGUI;
 
 /* @variables partagées */
 extern int etatCommMoniteur;
+extern int etatCommRobot;
 extern int img_transmit;
 extern DServer *serveur;
 extern DRobot *robot;
 extern DMovement *move;
 extern DArena *arena;
-//extern DBattery *battery;
+extern DBattery *battery;
+extern DImage *image;
 //extern DPosition *position;
 
 /* @constantes */
@@ -65,8 +64,8 @@ extern int PRIORITY_TCONNECT;
 extern int PRIORITY_TMOVE;
 extern int PRIORITY_TENVOYER;
 extern int PRIORITY_TIMAGE;
-//extern int PRIORITY_TARENE;
-//extern int PRIORITY_TBATTERY;
+extern int PRIORITY_TARENE;
+extern int PRIORITY_TBATTERY;
 //extern int PRIORITY_TFERMER_ROBOT;
 //extern int PRIORITY_TPOSITION;
 
